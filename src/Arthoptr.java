fbf7a69917c16c9c3771040e7dceee8ee1ef9618
Raj Varma <itzrajvarma@gmail.com>
2022-06-20 19:41:07 +0530

    Hi New Java Series is here V1

---
+++ src/Arthoptr.java
@@ -0,0 +1,12 @@
+public class Arthoptr {
+
+    public static void main(String[] args) {
+        int a,b,c;
+        b = 25;
+        c = 25;
+        a = b + c;
+
+        System.out.println("The Value of A is : "+a);
+    }
+
+}
